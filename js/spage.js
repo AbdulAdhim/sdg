49fbf439c6af03b244228850bcda8b870bc09117
xyaoo99 <xyaoo99@gmail.com>
2020-12-26 02:06:01 +0900

    Single Page (Home Page function done)

---
+++ js/spage.js
@@ -0,0 +1,20 @@
+// TODO: show "How-to-Play" text when mouse hovered over qmark
+
+const bodyTag = document.getElementById("bodyclick");
+
+bodyTag.addEventListener("click", event => {
+
+
+    if (event.target.id == 'qmark') {
+        console.log("Clicked on htp");
+        $('#htpModal').modal({show: true})
+    } else if (event.target.id == 'bodyclick') {
+        console.log("Clicked on body");
+        $('#RoomModal').modal({show: true})
+    } else {
+        console.log("Nothing");
+    }
+    
+})
+
+
